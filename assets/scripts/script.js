4108bcee0596b8961bf5eeb78c4bb6776f27eb76
SofaKB <hall.b17@gmail.com>
2019-12-26 11:20:31 -0500

    added checkbox and pw length selectors

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var symbols = ["!","@","#","$","%","^","&","*","(",")","-","+","<",">"]
+
+function getUpper() {
+    return String.indexOf(Math.floor(Math.random) * 26)
+    
+    console.log(getUpper())
+
+}    
