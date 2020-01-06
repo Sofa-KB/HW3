30374dfaba11ce4073f75bd89e01fc3cfefc763a
SofaKB <hall.b17@gmail.com>
2020-01-06 16:42:10 -0500

    added commentation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,10 +5,12 @@ var lower   = ["abcdefghijklmnopqrstuvwxyz"];
 var numbers = ["1234567890"];
 var symbols = ["!@#$%^&*()_+?><"];
 
+// Password Create Function
 function createPW(pwLength, characters){ 
 
 var pw = '';
 
+// Do loop to check for correct numerical input
 do {
     pwLength = parseInt(prompt("Enter a value between 8 and 128"));
 
@@ -24,8 +26,11 @@ else if (pwLength < 8 || pwLength > 128) {
 
 while (pwLength < 8 || pwLength > 128);
 
+// character declaration for usable password characters
 var characters = '';
 
+
+//Do loop to check for character types, requiring at least one character type
 do {
     hasSymbols = confirm("Include symbols? OK for Yes, Cancel for No");
     hasNumbers = confirm("Include numbers? OK for Yes, Cancel for No");
@@ -54,15 +59,16 @@ do {
                 characters += upper;
             }
 
-
+// For loop to randomize character choice based on chosen length            
     for (var i = 0; i < pwLength; i++){
         pw += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
+// Push final pw to page    
     document.getElementById("displayBox").innerText = pw; 
-    
 }  
 
+// Clipboard function to copy password
 function clipboard() {
     var copyText = document.getElementById("displayBox");
     navigator.clipboard.writeText(copyText.innerText);
